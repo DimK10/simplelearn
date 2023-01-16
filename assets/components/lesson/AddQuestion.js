478049f6bd8b0bb02f5815d780db608087fc42b0
DimK10 <james_k23@hotmail.gr>
2023-01-16 13:52:38 +0200

    Adds functionality to delete button in saved questions, removes static content in questions - edit button has no functionality yet

--- assets/components/lesson/AddQuestion.js
+++ assets/components/lesson/AddQuestion.js
@@ -1,7 +1,20 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import PropTypes from "prop-types";
 
-const AddQuestion = ({ questionId, onRemoveQuestionClick }) => {
+const AddQuestion = ({questionId, onAddQuestionClick, onRemoveQuestionComponentClick}) => {
+
+    const [question, setQuestion] = useState({
+        id: questionId,
+        title: '',
+        checked: false
+    });
+
+    const onChange = (e) =>
+        setQuestion({...question, [e.target.name]: e.target.value});
+
+    const onCheckboxChange = (e) =>
+        setQuestion({...question, [e.target.name]: e.target.checked});
+
     return (
         <Fragment>
             <div className="card mb-4">
@@ -10,32 +23,35 @@ const AddQuestion = ({ questionId, onRemoveQuestionClick }) => {
                         <div className="row w-100">
                             <div className="col-sm-5 col-md-6">
                                 <div className="form-outline">
-                                    <input type="text" id="form5Example1" className="form-control"/>
+                                    <input type="text" id="form5Example1" className="form-control" name="title"
+                                           onChange={(e) => onChange(e)} required={true}/>
                                     <label className="form-label" htmlFor="form5Example1">Enter Your Answer</label>
                                 </div>
                             </div>
                             <div className="col-sm-3 col-md-2 answer-checkbox">
                                 <div className="form-check d-flex text-center">
-                                    <input className="form-check-input me-2" type="checkbox" value="" id="form5Example3"
-                                           defaultChecked={true}/>
+                                    <input className="form-check-input me-2" type="checkbox" name="checked"
+                                           id="form5Example3"
+                                           defaultChecked={false} onChange={(e) => onCheckboxChange(e)}
+                                    />
                                     <label className="form-check-label" htmlFor="form5Example3">
                                         Correct
                                     </label>
                                 </div>
                             </div>
                             <div className="col-sm-2 col-md-2">
-                                <button type="submit" className="btn btn-success w-100">
+                                <button type="submit" className="btn btn-success w-100"
+                                        onClick={(e) => onAddQuestionClick(e, questionId, question)}>
                                     <i className="fa-solid fa-check"></i>
                                 </button>
                             </div>
                             <div className="col-sm-2 col-md-2">
-                                <button type="button" className="btn btn-danger w-100" onClick={() => {onRemoveQuestionClick(questionId)}} >
+                                <button type="button" className="btn btn-danger w-100"
+                                        onClick={() => onRemoveQuestionComponentClick(questionId)}>
                                     <i className="fa-solid fa-x"></i>
                                 </button>
                             </div>
                         </div>
-
-
                     </form>
                 </div>
             </div>
@@ -45,7 +61,8 @@ const AddQuestion = ({ questionId, onRemoveQuestionClick }) => {
 
 AddQuestion.proptypes = {
     questionId: PropTypes.string.isRequired,
-    onRemoveQuestionClick: PropTypes.func.isRequired
+    onAddQuestionClick: PropTypes.func.isRequired,
+    onRemoveQuestionComponentClick: PropTypes.func.isRequired
 }
 
 export default AddQuestion;
--- assets/components/lesson/LessonForm.js
+++ assets/components/lesson/LessonForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from "prop-types";
 import AddQuestion from "./AddQuestion";
 import {v4 as uuidv4} from 'uuid';
@@ -15,6 +15,12 @@ const LessonForm = ({lesson}) => {
         questions
     } = formData;
 
+    useEffect(() => {
+        console.log(formData);
+    }, [formData]);
+
+    const formIsInvalid = (form) => form.checkValidity() === false;
+
     const onPlusBtnClick = (e) => {
         e.preventDefault();
 
@@ -25,8 +31,42 @@ const LessonForm = ({lesson}) => {
         setQuestionComponents([...questionComponents, questionComponent])
     }
 
-    const onRemoveQuestionClick = (questionId) => {
+    const onAddQuestionClick = (e, questionId, question) => {
+        e.preventDefault();
+        const form = e.target.form;
+
+        // Check for basic form validity
+        if (form.checkValidity() === false) {
+            form.reportValidity();
+            return;
+        }
+        // TODO SEND TO API
+
+        // add to formData
+        setFormData({
+            ...formData,
+            questions: [...questions, question]
+        });
+
+        // remove from questions
+        onRemoveQuestionComponentClick(questionId);
+    }
+
+    const removeSavedQuestion= (questionId) => {
+        setFormData({...formData, questions: [...questions.filter((question) => question.id !== questionId)]});
+    }
+
+    const onRemoveQuestionComponentClick = (questionId) => {
         setQuestionComponents([...questionComponents.filter((questionComponent) => questionComponent.id !== questionId)]);
+    }
+
+
+    const onRemoveSavedQuestionClick = (e) => {
+        const questionId = e.currentTarget.getAttribute('data-index');
+
+        // TODO SEND TO API REMOVE ANSWER
+
+        removeSavedQuestion(questionId);
     }
 
     return (
@@ -42,75 +82,46 @@ const LessonForm = ({lesson}) => {
                             </p>
                         </div>
                         <div className="card-body">
-                            <div className="row mb-3">
-                                <div className="card">
-                                    <div
-                                        className="card-body d-flex justify-content-between align-items-center question-card-body">
-                                        <p className="card-text me-auto">1) Black my brother</p>
-                                        <button type="button" className="btn btn-warning m-1"
-                                                style={{height: 'min-content'}}
-                                        >
-                                            <i className="fa-solid fa-pen-to-square"
-                                               style={{display: 'inline', marginRight: '.3rem'}}></i>
-                                            <p style={{display: 'inline'}}>Edit</p>
-                                        </button>
-                                        <button type="button" className="btn btn-danger m-1"
-                                                style={{height: 'min-content'}}>
-                                            <i className="fa-solid fa-trash"
-                                               style={{display: 'inline', marginRight: '.3rem'}}></i>
-                                            <p style={{display: 'inline'}}>Delete</p>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row mb-3">
-                                <div className="card" id="correct">
-                                    <div
-                                        className="card-body d-flex justify-content-between align-items-center question-card-body">
-                                        <p className="card-text me-auto">2) Sanguine my brother</p>
-                                        <button type="button" className="btn btn-warning m-1"
-                                                style={{height: 'min-content'}}
-                                        >
-                                            <i className="fa-solid fa-pen-to-square"
-                                               style={{display: 'inline', marginRight: '.3rem'}}></i>
-                                            <p style={{display: 'inline'}}>Edit</p>
-                                        </button>
-                                        <button type="button" className="btn btn-danger m-1"
-                                                style={{height: 'min-content'}}>
-                                            <i className="fa-solid fa-trash"
-                                               style={{display: 'inline', marginRight: '.3rem'}}></i>
-                                            <p style={{display: 'inline'}}>Delete</p>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row mb-3">
-                                <div className="card">
-                                    <div
-                                        className="card-body d-flex justify-content-between align-items-center question-card-body">
-                                        <p className="card-text me-auto">3) Red my brother</p>
-                                        <button type="button" className="btn btn-warning m-1"
-                                                style={{height: 'min-content'}}
-                                        >
-                                            <i className="fa-solid fa-pen-to-square"
-                                               style={{display: 'inline', marginRight: '.3rem'}}></i>
-                                            <p style={{display: 'inline'}}>Edit</p>
-                                        </button>
-                                        <button type="button" className="btn btn-danger m-1"
-                                                style={{height: 'min-content'}}>
-                                            <i className="fa-solid fa-trash"
-                                               style={{display: 'inline', marginRight: '.3rem'}}></i>
-                                            <p style={{display: 'inline'}}>Delete</p>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
+                            {
+                                questions !== null
+                                &&
+                                questions.length > 0
+                                    ?
+                                    questions.map(question => (
+                                        <div className="row mb-3" key={question.id}>
+                                            <div className={`card ${question.checked && 'correct'}`}>
+                                                <div
+                                                    className="card-body d-flex justify-content-between align-items-center question-card-body">
+                                                    <p className="card-text me-auto">{questions.indexOf(question) + 1}) {question.title}</p>
+                                                    <button type="button" className="btn btn-warning m-1"
+                                                            style={{height: 'min-content'}}
+                                                    >
+                                                        <i className="fa-solid fa-pen-to-square"
+                                                           style={{display: 'inline', marginRight: '.3rem'}}></i>
+                                                        <p style={{display: 'inline'}}>Edit</p>
+                                                    </button>
+                                                    <button type="button" className="btn btn-danger m-1"
+                                                            style={{height: 'min-content'}} data-index={question.id}
+                                                            onClick={(e) => onRemoveSavedQuestionClick(e)}>
+                                                        <i className="fa-solid fa-trash"
+                                                           style={{display: 'inline', marginRight: '.3rem'}}></i>
+                                                        <p style={{display: 'inline'}}>Delete</p>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                    :
+                                    <h5 className="text-center">No questions Added! Press the plus button to add a new
+                                        question</h5>
+                            }
                             <div className="row">
                                 {
                                     questionComponents.map((questionComponent) => {
                                         return <AddQuestion questionId={questionComponent.id}
                                                             key={questionComponent.id}
-                                                            onRemoveQuestionClick={onRemoveQuestionClick}
+                                                            onAddQuestionClick={onAddQuestionClick}
+                                                            onRemoveQuestionComponentClick={onRemoveQuestionComponentClick}
                                         />
                                     })
                                 }
